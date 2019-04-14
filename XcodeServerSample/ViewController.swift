3a5e3e31ff022d0ef19b41490a019d94d665493b
satya <reddy.satyan@gmail.com>
2019-04-14 16:53:07 -0400

    First comm

---
+++ XcodeServerSample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XcodeServerSample
+//
+//  Created by satya on 4/14/19.
+//  Copyright © 2019 satya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + 2
+    }
+
+
+}
+
---
+++ XcodeServerSampleTests/XcodeServerSampleTests.swift
@@ -0,0 +1,37 @@
+//
+//  XcodeServerSampleTests.swift
+//  XcodeServerSampleTests
+//
+//  Created by satya on 4/14/19.
+//  Copyright © 2019 satya. All rights reserved.
+//
+
+import XCTest
+@testable import XcodeServerSample
+
+class XcodeServerSampleTests: XCTestCase {
+
+    
+    var viewController: ViewController!
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewController = ViewController()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(viewController.addNumbers(num1: 2, num2: 3), 5)
+    }
+    
+    func testAdd2() {
+        XCTAssertEqual(viewController.addNumbers(num1: 5, num2: 3), 8)
+    }
+    
+    func testAdd3() {
+        XCTAssertNotEqual(viewController.addNumbers(num1: 2, num2: 3), 4)
+    }
+
+}
